ea858025d2c18b53f43f51195d0fae8e222772cb
alvinend <alvinendratno@live.com>
2023-03-05 19:50:44 +0900

    Fix Preview

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
+import { StyleSheetManager } from 'styled-components'
 
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
+  const iframe = document.querySelector("#preview-pane")
+  const iframeHeadElem = iframe.contentDocument.head
 
   if (data) {
     return (
-      <IndexPageTemplate
-        linkShopee={data.linkShopee}
-        linkTokopedia={data.linkTokopedia}
-        linkInstagram={data.linkInstagram}
-        linkFacebook={data.linkFacebook}
-        linkWhatsapp={data.linkWhatsapp}
-        whatsAppNumber={data.whatsAppNumber}
-        email={data.email}
-        jumbotronMetadata={data.jumbotron}
-        featuresMetadata={data.features}
-        strongPointsMetadata={data.strongPoints}
-        testimoniesMetadata={data.testimonies}
-        supportMetadata={data.support}
-      />
+      <StyleSheetManager target={iframeHeadElem}>
+        <IndexPageTemplate
+          linkShopee={data.linkShopee}
+          linkTokopedia={data.linkTokopedia}
+          linkInstagram={data.linkInstagram}
+          linkFacebook={data.linkFacebook}
+          linkWhatsapp={data.linkWhatsapp}
+          whatsAppNumber={data.whatsAppNumber}
+          email={data.email}
+          jumbotronMetadata={data.jumbotron}
+          featuresMetadata={data.features}
+          strongPointsMetadata={data.strongPoints}
+          testimoniesMetadata={data.testimonies}
+          supportMetadata={data.support}
+        />
+      </StyleSheetManager>
     )
   } else {
     return <div>Loading...</div>
